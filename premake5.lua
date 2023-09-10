6a27ad8ee1f878b465f95c37cc1cb386f70d64df
aPenguin360 <53341222+aPenguin360@users.noreply.github.com>
2023-09-10 09:57:07 -0500

    Updated formatting to LLVM and Implemented Premake build system

---
+++ premake5.lua
@@ -0,0 +1,97 @@
+workspace "Pressia"
+	architecture "x64"
+
+	configurations {
+		"Debug",
+		"Release",
+		"Dist",
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Pressia"
+	location "Pressia"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs {
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines {
+			"PS_PLATFORM_WINDOWS",
+			"PS_BUILD_DLL"
+		}
+
+		postbuildcommands {
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "PS_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "PS_RELEASE"
+		symbols "On"
+
+	filter "configurations:Dist"
+		defines "PS_DIST"
+		symbols "On"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs {
+		"Pressia/vendor/spdlog/include",
+		"Pressia/src"
+	}
+
+	links {
+		"Pressia"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines {
+			"PS_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "PS_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "PS_RELEASE"
+		symbols "On"
+
+	filter "configurations:Dist"
+		defines "PS_DIST"
+		symbols "On"
